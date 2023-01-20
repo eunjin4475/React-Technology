d1b2260ff4d624d5dce8d975a5c0f2769c12234e
eunjin17 <90459652+eunjin4475@users.noreply.github.com>
2023-01-20 12:42:54 +0900

    usestate사용하기

--- hi-react/src/Counter.js
+++ hi-react/src/Counter.js
@@ -4,23 +4,25 @@ class Counter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      number: 0
+      number : 0,
+      fixedNumber: 0
     };
   }
   render() {
-    const { number } = this.state;
+    const { number, fixedNumber } = this.state;
     return (
       <div>
         <h1>{number}</h1>
-        <button 
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
+        <button
         onClick={() => {
-          this.setState({ number: number + 1 });
+          this.setState({number: number + 1 }); // this.setState를 설정해줘야 값을 바꿔준다.
         }}
         >
           +1
         </button>
       </div>
-    );
+    )
   }
 }
 
